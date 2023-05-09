c1309b01b41aef7bb80149e8ce3fd3e829624c52
vygral <64483118+vygral@users.noreply.github.com>
2023-05-09 16:55:58 +0200

    Update database.inc.php

--- Chatos/inc/database.inc.php
+++ Chatos/inc/database.inc.php
@@ -8,243 +8,84 @@ class Database{
 
 
     public function __construct(string $serverName = "localhost", string $dbUserName = "root", string $dbPassword = "", string $dbName = "Chatos"){
-        $this->connection = mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);
-        if(!$this->connection){
-            throw new Exception("Could not connect to database");
-        }
+        //error is thrown if connection is not established
+        $this->connection = new PDO("mysql:host=$serverName;dbname=$dbName", $dbUserName, $dbPassword);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->serverName = $serverName;
         $this->dbUserName = $dbUserName;
         $this->dbPassword = $dbPassword;
         $this->dbName = $dbName;
     }
 
-    public function getMessages(int $idc){
-        $sql = "SELECT * FROM messages WHERE chat = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-        
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            throw new Exception("Failed to prepare the statement");
-        }
-        
-        mysqli_stmt_bind_param($stmt, "i", $idc);
-        mysqli_stmt_execute($stmt);
-
-        return mysqli_stmt_get_result($stmt);
-
+    public function getMessages(int $chatgroupid){
+        $stmt = $this->connection->prepare("SELECT idu, text from messages where idc = :idc");
+        $stmt->execute(["idc" => $chatgroupid]);
+        return $stmt->fetchAll();
     }
 
-    public function getChatList(string $idu, int $accepted){
-        $sql = "SELECT users.username, chats.IDc, chats.acceptor FROM users, chats, usersxchats WHERE users.IDu = usersxchats.IDu AND usersxchats.IDc = chats.IDc 
-        AND users.IDu != ? AND (chats.applicant = ? OR chats.acceptor = ?) AND chats.accepted = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "sssi", $idu, $idu, $idu, $accepted);
-        mysqli_stmt_execute($stmt);
-
-        return mysqli_stmt_get_result($stmt);
+    public function getChatList(int $idu){
+        $stmt = $this->connection->prepare("SELECT c.name from userchatgroups uc join chatgroups c on(uc.idc = c.idc) where ud.idu = :idu");
+        $stmt->execute(["idu" => $idu]);
+        return $stmt->fetchAll();
     }
 
-    function emailExists($email) {
-        $sql = "SELECT * FROM users WHERE userEmail = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare statement");
-        }
-        mysqli_stmt_bind_param($stmt, "s", $email);
-        mysqli_stmt_execute($stmt);
-    
-        $resultData = mysqli_stmt_get_result($stmt);
-    
-        if($row = mysqli_fetch_assoc($resultData)) {
-            return $row;
-        }
-        else {
-            return false;
-        }
+    public function createUser(string $nickname, string $password) {
+        $hash = password_hash($password, PASSWORD_DEFAULT);//password_verify()
+        $stmt = $this->connection->prepare("insert into users (nickname, password) values (:nickname, :password)");
+        $stmt->execute(["nickname" => $nickname, "password" => $hash]);
+    }
+
+    public function createChatGroup(string $groupName){
+        $stmt = $this->connection->prepare("insert into chatgroups (name) values (:groupName)");
+        $stmt->execute(["name" => $groupName]);
+    }
+
+    public function createDirectChatGroup(string $groupName, int $idu1, int $idu2){
+        $stmt = $this->connection->prepare("insert into chatgroups (name) values (:groupName)");
+        $stmt->execute(["name" => $groupName]);
+        //$stmt = $this->connection->prepare("insert into userchatgroups (idu, idc
     }
     
     function chatExists($applicant, $acceptor) {
-        $sql = "SELECT * FROM chats WHERE (applicant = ? AND acceptor = ?) OR (acceptor = ? AND applicant = ?);";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare statement");
-        }
-        mysqli_stmt_bind_param($stmt, "ssss", $applicant, $acceptor, $applicant, $acceptor);
-        mysqli_stmt_execute($stmt);
-    
-        $resultData = mysqli_stmt_get_result($stmt);
-    
-        if(mysqli_fetch_assoc($resultData)) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        
     }
 
     function deleteChat($IDc) {
-        $sql = "DELETE FROM chats WHERE IDc = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare statement");
-        }
-        mysqli_stmt_bind_param($stmt, "i", $IDc);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-    
-        $sql = "DELETE FROM usersxchats WHERE IDc = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare statement");
-        }
-        mysqli_stmt_bind_param($stmt, "i", $IDc);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
+        
     }
 
     public function uidExists($username) {
-        $sql = "SELECT * FROM users WHERE username = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "s", $username);
-        mysqli_stmt_execute($stmt);
-    
-        $resultData = mysqli_stmt_get_result($stmt);
-    
-        if($row = mysqli_fetch_assoc($resultData)) {
-            return $row;
-        }
-        else {
-            return false;
-        }
+        
     }
 
     function createChat($applicant, $acceptor) {
-        $sql = "INSERT INTO chats (applicant, acceptor, accepted) VALUES (?, ?, 'false');";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "ii", $applicant, $acceptor);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-    
-        $sql = "SELECT IDc FROM chats WHERE applicant = ? AND acceptor = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "ii", $applicant, $acceptor);
-        mysqli_stmt_execute($stmt);
-        $IDc = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
-        mysqli_stmt_close($stmt);
-    
-        $sql = "INSERT INTO usersxchats (IDu, IDc) VALUES (?, ?);";
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "ii", $applicant, $IDc["IDc"]);
-        mysqli_stmt_execute($stmt);
         
-        $stmt = mysqli_stmt_init($this->connection);
-    
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "ii", $acceptor, $IDc["IDc"]);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-        
-        //should not be here
-        /*
-        header("location: ../index.php");
-        exit();
-        */
     }
     
     
-    function createUser($username, $email, $pwd) {
-        $sql = "INSERT INTO users (username, userEmail, userPwd, UserRole) VALUES (?, ?, ?, 'pleb');";
-        $stmt = mysqli_stmt_init($this->connection);
-
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-
-        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-
-        mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-        
-        //should not be here
-        /*
-        loginUser($this->connection, $username, $pwd);
-        */
-    }
 
     function getComplaints(){
-        $sql = "SELECT * FROM messages WHERE chat = -1 ORDER BY IDm DESC";
-        $stmt = mysqli_stmt_init($this->connection);
         
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_execute($stmt);
-
-        return mysqli_stmt_get_result($stmt);
     }
 
     function addMessage($idc, $idu, $message){
-        $sql = "INSERT INTO messages (chat, user, message, edited) VALUES (?, ?, ?, 0);";
-        $stmt = mysqli_stmt_init($this->connection);
-
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "iis", $idc, $idu, $message);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
+        
     }
 
     public function newComplaint($idc, $idu, $complaint){
-        $sql = "INSERT INTO messages (chat, user, message, edited) VALUES (?, ?, ?, 0);";
-        $stmt = mysqli_stmt_init($this->connection);
-
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "iis", $_SESSION["idc"], $_SESSION["idu"], $_POST["complaint"]);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
+        
     }
 
     public function acceptChat($idc){
-        $sql = "UPDATE chats set accepted = 1 WHERE IDc = ?;";
-        $stmt = mysqli_stmt_init($this->connection);
-
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            throw new Exception("Failed to prepare the statement");
-        }
-        mysqli_stmt_bind_param($stmt, "i", $idc);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
+        
     }
 
 }
 
+//testing functions
+/*
+$database = new Database();
+//print_r($database->getChatList(1));
+$database->createUser("TestUser", "heslo1234");
+*/
 ?>
